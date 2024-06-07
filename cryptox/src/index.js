fc30d981db3ca1a17b45ea159f94cab1dca4b322
Manin Gauba <manindgauba@gmail.com>
2024-06-07 15:46:15 -0400

    started react app

---
+++ cryptox/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes, } from 'react-router-dom';
+import Home from './pages/Home'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <BrowserRouter>
+ <Routes>
+  <Route index element={<Home />} />
+ </Routes>
+ </BrowserRouter>
+);
+
